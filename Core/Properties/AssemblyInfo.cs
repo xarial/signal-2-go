f39479fe1c96415a4b2ae435d2b141cb930d50ee
Xarial <xarial@outlook.com>
2018-08-17 22:37:49 +1000

    Signed assemblies and added nuget

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: InternalsVisibleTo("CoreTests")]
+[assembly: InternalsVisibleTo("CoreTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010071e2da475a61a8270f2fd18b1589d8875d8727553636f913cf86006258e02e17ce917d00e966c1600cdf2bc485015afe36e5661639c21e3c4279b3f0aa8c73cce80a88a9f6a85f8fa77d8f2dd37de1a01daeea5872f37daa2c638c98d20115d29f53e475f69423c6e427084d61a7dd65b0771c8f08925883d7ced74bffeb18ca")]
 
 [assembly: Guid("aa4345f8-2fc8-4b74-84f7-986e8b83b752")]
 
--- Services/UserSettings/Properties/AssemblyInfo.cs
+++ Services/UserSettings/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("fcb9f4ab-86fe-4dbc-8733-02238798a5d1")]
 
-[assembly: InternalsVisibleTo("CoreTests")]
+[assembly: InternalsVisibleTo("CoreTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010071e2da475a61a8270f2fd18b1589d8875d8727553636f913cf86006258e02e17ce917d00e966c1600cdf2bc485015afe36e5661639c21e3c4279b3f0aa8c73cce80a88a9f6a85f8fa77d8f2dd37de1a01daeea5872f37daa2c638c98d20115d29f53e475f69423c6e427084d61a7dd65b0771c8f08925883d7ced74bffeb18ca")]
 
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
