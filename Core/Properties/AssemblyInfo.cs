3b66f5ad9b53419b7f8c778bda62d73d56f2c9d0
Xarial <xarial@outlook.com>
2018-10-07 10:51:32 +1100

    Removed key signing

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -13,9 +13,9 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: InternalsVisibleTo("CoreTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010071e2da475a61a8270f2fd18b1589d8875d8727553636f913cf86006258e02e17ce917d00e966c1600cdf2bc485015afe36e5661639c21e3c4279b3f0aa8c73cce80a88a9f6a85f8fa77d8f2dd37de1a01daeea5872f37daa2c638c98d20115d29f53e475f69423c6e427084d61a7dd65b0771c8f08925883d7ced74bffeb18ca")]
+[assembly: InternalsVisibleTo("CoreTests")]
 
 [assembly: Guid("aa4345f8-2fc8-4b74-84f7-986e8b83b752")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
